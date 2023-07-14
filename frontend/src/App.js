d5e73a001803b76ecd5526928a04c56d2a454af3
Gabor Tamasko <117508570+tamaskogabo@users.noreply.github.com>
2023-07-14 15:42:34 +0200

    fetch and display the data in the CalculatePage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,54 @@
 import './App.css';
 import CalculatePage from './Components/CalculatePage';
 import TopMenu from './Components/TopMenu';
+import { useState, useEffect } from 'react';
+
+const MONTHS = [
+    'JANUARY',
+    'FEBRUARY',
+    'MARCH',
+    'APRIL',
+    'MAY',
+    'JUNE',
+    'JULY',
+    'AUGUST',
+    'SEPTEMBER',
+    'OCTOBER',
+    'NOVEMBER',
+    'DECEMBER',
+];
 
 function App() {
+    const [lastMonthData, setlastMonthData] = useState(null);
+
+    useEffect(() => {
+        async function fetchLastMonth() {
+            setlastMonthData(null);
+            const currentYear = new Date().getFullYear();
+            
+            //Has to be set one month earlier after everything is done!
+            const currentMonth = MONTHS[new Date().getMonth()];
+            console.log(currentYear);
+            console.log(currentMonth);
+            const response = await fetch(
+                `http://localhost:8080/clockstands/date?year=${currentYear}&month=${currentMonth}`,
+            );
+            const result = await response.json();
+            if (!ignore) {
+                setlastMonthData(result);
+            }
+        }
+        let ignore = false;
+        fetchLastMonth();
+        return () => {
+            ignore = true;
+        };
+    }, []);
+
     return (
         <>
             <TopMenu />
-            <CalculatePage />
+            <CalculatePage lastMonthData={lastMonthData} />
         </>
     );
 }
--- frontend/src/Components/CalculatePage.jsx
+++ frontend/src/Components/CalculatePage.jsx
@@ -4,27 +4,30 @@ import WaterDropIcon from '@mui/icons-material/WaterDrop';
 import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
-export default function CalculatePage() {
+export default function CalculatePage({ lastMonthData }) {
+
+    //TODO loading state for the fetch of data
+    
     return (
         <>
             <Stack direction='column' alignItems='center'>
-                <Box width={0.9} mt='20px'>
+                <Box maxWidth={0.9} mt='20px'>
                     <Paper elevation={3}>
                         <Typography variant='h5' p={2} mt='20px'>
-                            <WaterDropIcon sx={{marginRight : '10px'}}/>
-                            Last month's warm water:
+                            <WaterDropIcon sx={{ marginRight: '10px' }} />
+                            Last month's warm water: {lastMonthData && lastMonthData[0].warmWaterStand}
                         </Typography>
                         <Typography variant='h5' p={2}>
-                            <WaterDropIcon sx={{marginRight : '10px'}}/>
-                            Last month's cold water:
+                            <WaterDropIcon sx={{ marginRight: '10px' }} />
+                            Last month's cold water: {lastMonthData && lastMonthData[0].coldWaterStand}
                         </Typography>
                         <Typography variant='h5' p={2}>
-                            <ElectricBoltIcon sx={{marginRight : '10px'}}/>
-                            Last month's electricity:
+                            <ElectricBoltIcon sx={{ marginRight: '10px' }} />
+                            Last month's electricity: {lastMonthData && lastMonthData[0].electricityStand}
                         </Typography>
                         <Typography variant='h5' p={2} mb='20px'>
-                            <WbSunnyIcon sx={{marginRight : '10px'}}/>
-                            Last month's warming:
+                            <WbSunnyIcon sx={{ marginRight: '10px' }} />
+                            Last month's warming: {lastMonthData && lastMonthData[0].warmingBill} Ft
                         </Typography>
                     </Paper>
                 </Box>
