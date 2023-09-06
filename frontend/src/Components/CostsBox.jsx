ae4c248233512c022d934b479e7de4affda21abe
Gabor Tamasko <117508570+tamaskogabo@users.noreply.github.com>
2023-09-06 11:24:52 +0200

    refactor calculation function on frontend

--- frontend/src/Components/CostsBox.jsx
+++ frontend/src/Components/CostsBox.jsx
@@ -4,56 +4,39 @@ import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import CallSplitIcon from '@mui/icons-material/CallSplit';
 
 export default function CostsBox({ thisMonthData, lastMonthData }) {
+    const WARM_WATER =
+        Math.floor(thisMonthData[0].warmWaterStand) -
+        Math.floor(lastMonthData[0].warmWaterStand);
+
+    const COLD_WATER =
+        Math.floor(thisMonthData[0].coldWaterStand) -
+        Math.floor(lastMonthData[0].coldWaterStand);
+
     function calculateMonthlyCosts() {
         const currentMonth = new Date().getMonth() + 1;
         if (currentMonth % 2 !== 0 && currentMonth % 3 === 0) {
-            const warmWater =
-                Math.floor(thisMonthData[0].warmWaterStand) -
-                Math.floor(lastMonthData[0].warmWaterStand);
-            const coldWater =
-                Math.floor(thisMonthData[0].coldWaterStand) -
-                Math.floor(lastMonthData[0].coldWaterStand);
-
-            return (
-                thisMonthData[0].baseRent +
-                thisMonthData[0].kkt +
-                thisMonthData[0].warmingBill +
-                thisMonthData[0].gasBill +
-                (thisMonthData[0].electricityStand -
-                    lastMonthData[0].electricityStand) *
-                    38.7 +
-                (warmWater + coldWater) * 600 +
-                338
-            );
+            return calculateOddAndQuarterlyCosts(WARM_WATER, COLD_WATER);
         } else if (currentMonth % 3 === 0) {
-            return (
-                thisMonthData[0].baseRent +
-                thisMonthData[0].kkt +
-                thisMonthData[0].warmingBill +
-                thisMonthData[0].gasBill +
-                (thisMonthData[0].electricityStand -
-                    lastMonthData[0].electricityStand) *
-                    38.7
-            );
+            return calculateQuarterlyCost();
         } else if (currentMonth % 2 !== 0) {
-            const warmWater =
-                Math.floor(thisMonthData[0].warmWaterStand) -
-                Math.floor(lastMonthData[0].warmWaterStand);
-            const coldWater =
-                Math.floor(thisMonthData[0].coldWaterStand) -
-                Math.floor(lastMonthData[0].coldWaterStand);
-
-            return (
-                thisMonthData[0].baseRent +
-                thisMonthData[0].kkt +
-                thisMonthData[0].warmingBill +
-                (thisMonthData[0].electricityStand -
-                    lastMonthData[0].electricityStand) *
-                    38.7 +
-                (warmWater + coldWater) * 600 +
-                338
-            );
+            return calculateOddMonthCost(WARM_WATER, COLD_WATER);
         }
+        return calculateSimpleMonth();
+    }
+
+    function calculateOddAndQuarterlyCosts(warmWater, coldWater) {
+        return calculateOddMonthCost(warmWater, coldWater) + thisMonthData[0].gasBill;
+    }
+
+    function calculateQuarterlyCost() {
+        return calculateSimpleMonth() + thisMonthData[0].gasBill;
+    }
+
+    function calculateOddMonthCost(warmWater, coldWater) {
+        return calculateSimpleMonth() + (warmWater + coldWater) * 600 + 338;
+    }
+
+    function calculateSimpleMonth() {
         return (
             thisMonthData[0].baseRent +
             thisMonthData[0].kkt +
@@ -65,11 +48,11 @@ export default function CostsBox({ thisMonthData, lastMonthData }) {
     }
 
     let sumToWire = 0;
-    let sharedCosts = 6500;
+    let sharedCostsWithInternet = 6500;
 
     if (thisMonthData) {
         sumToWire += calculateMonthlyCosts();
-        sharedCosts += calculateMonthlyCosts();
+        sharedCostsWithInternet += calculateMonthlyCosts();
     }
 
     return (
@@ -80,7 +63,7 @@ export default function CostsBox({ thisMonthData, lastMonthData }) {
             </Typography>
             <Typography variant='h5' p={2}>
                 <CallSplitIcon sx={{ marginRight: '10px' }} />
-                Sum to ask from flatmate: {sharedCosts / 2}
+                Sum to ask from flatmate: {sharedCostsWithInternet / 2}
             </Typography>
         </>
     );
